46b06a78967dc3c266722c31d98d6953f2fd3afd
andel-vitorio <andelvitorio10@gmail.com>
2023-02-24 12:30:20 -0400

    feat: add echo command

--- ShellProject.cpp
+++ ShellProject.cpp
@@ -28,6 +28,26 @@ static const std::string ANSI_COLOR_CYAN = "\x1b[36m";
 static const std::string ANSI_COLOR_WHITE = "\x1b[37m";
 static const std::string ANSI_COLOR_RESET = "\x1b[0m";
 
+
+/**
+ * Remove os espaços em branco do início e fim
+ * de uma string.
+ * 
+ * @param[in] str Uma string com espaços em branco no início e/ou fim.
+ * @return Um string sem espaços em branco no início e no fim.
+*/
+std::string trim(const std::string & str) {
+    const auto begin = str.find_first_not_of(" \t");
+
+    if ( begin == std::string::npos )
+        return "";
+
+    const auto end = str.find_last_not_of(" \t");
+    const auto len = end - begin + 1;
+    
+    return str.substr(begin, len);
+}
+
 /**
  * Escopos das funções responsáveis em executar
  * os comandos disponíveis.
@@ -84,6 +104,25 @@ namespace Runner {
  * É utilizada para representar um shell simples.
 */
 class Shell {
+
+    private:
+
+    /**
+     * Obtém argumento do comando 'echo' a partir de um texto.
+     * 
+     * @param[in] text O texto com o comando 'echo' e seu parâmetro.
+     * @return O argumento do comando 'echo'. 
+    */
+    std::string getEchoArg(std::string text) {
+        std::string arg;
+
+        size_t pos = text.find("echo");
+
+        if ( pos != std::string::npos )
+            text.erase(pos, 4);
+
+        return trim(text);
+    }
     
     public: 
 
@@ -136,11 +175,16 @@ class Shell {
     void runCommandFromText(std::string text) {
 
         // Comando de saída do shell
-        if ( std::regex_match(text, std::regex("(\\s*)(exit|quit)(\\s*)")) ) {
+        if ( std::regex_match(text, std::regex("(\\s*)(exit|quit)(\\s*)")) )
             isRunning = false;
-        } else {
-            Runner::display("Comando não encontrado: " + text, 'e');
-        }
+
+        // Comando para mostrar um texto
+        else if ( std::regex_match(text, std::regex("(\\s*)(echo)(\\s*)(.*)")) ) {
+            Runner::display(getEchoArg(text));
+        } 
+        
+        // Quando não é possível obter o comando do texto
+        else Runner::display("Comando não encontrado: " + text, 'e');
         
     }
 
